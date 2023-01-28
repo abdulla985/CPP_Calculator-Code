05183155eacbf032554a39af73cf72542578c09a
abdulla985 <122654462+abdulla985@users.noreply.github.com>
2023-01-28 21:17:01 +0300

    Create Caculater.cpp

---
+++ Caculater.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num1, num2;
+    cout << "zhmaray yakam bnusa: ";
+    cin >> num1;
+    cout << "zhmaray dwam bnusa: ";
+    cin >> num2;
+    cout << num1 << " x " << num2 << " = " << num1 * num2;
+    //Test caculater By Abdulla Hassan
+    return 0;
+}
